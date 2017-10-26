3d113ee9e20525eca375ca0d7a8b662c4a97349e
Padraig Gleeson <p.gleeson@gmail.com>
2017-10-26 18:54:47 +0100

    Earlier version antlr

---
+++ src/main/java/org/lemsml/exprparser/utils/TemporaryNeuroML2Helper.java
@@ -0,0 +1,16 @@
+/*
+ *
+ *
+ */
+package org.lemsml.exprparser.utils;
+
+import java.io.File;
+
+public class TemporaryNeuroML2Helper
+{
+    public static String extractExpressions(File nml2File)
+    {
+        return "??";
+    }
+    
+}
